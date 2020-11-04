fb1096d563a2ee0e55f3034f4c619d1ed1c9a3d6
Aleksandr Abramovskii <alex-abram@bk.ru>
2020-11-04 22:46:06 +0500

    Update index.blade.php

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -28,7 +28,7 @@
                         <option value="week" name="sort_by" class="sort_by" id="sort_by">Week</option>
                         <option value="future" name="sort_by" class="sort_by" id="sort_by">Future</option>
                         <option value="updated_at" name="sort_by" class="sort_by" id="sort_by" selected>Updated</option>
-                        @if(Auth::user()->isManager == true)
+                        @if(Auth::user()->is_manager == true)
                         <option value="assigned_to" name="sort_by" class="sort_by" id="sort_by">Employees</option>
                         @endif
                     </select>
